e57ca007310d2f4e005c1a5608ae5a440225140b
Will Peters <peterswi@bc.edu>
2020-05-21 18:52:04 -0500

    initial changes to template

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      This is the beginning of GiveHelp, our digital Case Management App
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          GiveCard Presents: GiveHelp
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.givecard.io/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit GiveCard
         </a>
       </header>
     </div>
