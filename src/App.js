48b446668b8e2203220b20afa0bb9755896f39db
Will Peters <peterswi@bc.edu>
2020-06-03 16:41:24 -0400

    working on linking with firebase

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> This the beginning of GiveHelp, our digital Case Management App</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h2>GiveCard Presents: GiveHelp</h2>
-        </p>
-        <a
-          className="App-link"
-          href="https://www.givecard.io/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visit GiveCard
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+//building this constructor and component did mount to grow within the app. Still missing something
+
+    constructor() {
+        super();
+        this.state= {
+            speed: 10
+        };
+    }
+    componentDidMount(){
+       const rootRef =firebase.database().ref().child('react');
+       const speedRef =rootRef.child('speed');
+       speedRef.on('value',snap=>{
+            this.setState({
+                speed: snap.val()
+        });
+       });
+    }
+
+    render()
+    {
+        return (
+            <div className="App">
+                <h1> This the beginning of GiveHelp, our digital Case Management App</h1>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        <h2>GiveCard Presents: GiveHelp</h2>
+                    </p>
+                    <p>
+                        <h2>React+firebase test: {this.state.speed} </h2>
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://www.givecard.io/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Visit GiveCard
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
