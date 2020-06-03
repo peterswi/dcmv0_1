7e34c4ee4ae8279d66064cac4cfa6ea4430d3609
Will Peters <peterswi@bc.edu>
2020-06-03 15:39:36 -0400

    corrected to setup firebase

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase as 'firebase';
 
+var firebaseConfig = {
+    apiKey: "AIzaSyDi5gs5uJ-BQBnmOY3MxW56m_6GlnzKVDs",
+    authDomain: "givecard-dcm.firebaseapp.com",
+    databaseURL: "https://givecard-dcm.firebaseio.com",
+    projectId: "givecard-dcm",
+    storageBucket: "givecard-dcm.appspot.com",
+    messagingSenderId: "252395130688",
+    appId: "1:252395130688:web:e17255fe0353cefefbb4a5",
+    measurementId: "G-ZRXDQVKKPB"
+  };
+
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
   <React.StrictMode>
     <App />
