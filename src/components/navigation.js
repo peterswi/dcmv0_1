75475c2d6f6e84211a818dbd2cd419c131b4d4fb
Will Peters <peterswi@bc.edu>
2020-06-10 16:46:45 -0400

    navigation page was the source of my navbar confusion

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,7 +5,11 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => {
     return (
        <div>
-          <NavLink to="/registerUser">Register New User</NavLink>
+           <NavLink to="/">HomePage</NavLink> <br></br>
+           <NavLink to="/registerUser">Register New User</NavLink><br></br>
+           <NavLink to="/orgPage">Oranization Page</NavLink><br></br>
+           <NavLink to="/contact">Contact GiveCard</NavLink><br></br>
+
        </div>
     );
 };
