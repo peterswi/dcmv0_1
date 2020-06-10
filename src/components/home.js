3310a7f041186bf0312005cbe2cc30e71719855c
Will Peters <peterswi@bc.edu>
2020-06-10 16:12:34 -0400

    design changes. placeholders for what is to come later on

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
+import {Link} from "react-router-dom";
 
 const home = () => {
     return (
          <div className="App">
-                <h1> GiveHelp</h1>
-                <h2> A tool for Case Management</h2>
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
+                    <h1> GiveCard Presents: GiveHelp</h1>
+                    <h2> A tool for Case Management</h2>
                     <p>
-                        <h2>GiveCard Presents: GiveHelp</h2>
                     </p>
+                </header>
+                <body>
                     <p>
+                        <h2><b> Click a link to visit our different pages</b></h2>
+                    </p>
+                    Visit an <Link to="/orgPage">Organization Page</Link><br></br>
+                    Add a <Link to="/registerUser">New User</Link>
+
+                </body>
+             <br></br><br></br>
+                <footer className="App-footer">
                     <a
                         className="App-link"
                         href="https://www.givecard.io/"
@@ -21,8 +31,11 @@ const home = () => {
                     >
                         Visit GiveCard
                     </a>
-                    </p>
-            </header>
+                    <b> OR </b>
+                        <Link to="/contact">Contact Us</Link>
+                    <br></br>
+                </footer>
+
     </div>
     );
 };
