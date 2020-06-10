ef0c401635a338d8ad64209d3d5714a9d40ef0b6
Will Peters <peterswi@bc.edu>
2020-06-10 16:46:27 -0400

    created this component to try and streamline things but dont have it working yet

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import home from './components/home';
+import RegisterUser from './components/registerUser';
+import orgPage from "./components/orgPage";
+import contact from './components/contact';
+import Error from "./components/error";
+import Navigation from "./components/navigation";
+
+const main = () =>{
+    return(
+
+        <Switch>
+             <Route exact path="/home" component={home} />
+             <Route path="/contact" component={contact}/>
+             <Route path="/registerUser" component={RegisterUser}/>
+             <Route path="/orgPage" component={orgPage}/>
+             <Route component={Error}/>
+        </Switch>
+    );
+};
+export default main;
