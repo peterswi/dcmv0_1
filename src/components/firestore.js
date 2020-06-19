7479ac9c755bfbf8ec576c151ff9dae381be11b3
Will Peters <peterswi@bc.edu>
2020-06-19 11:04:29 -0400

    better practice for firebase configuration

--- src/components/firestore.js
+++ src/components/firestore.js
@@ -1,20 +1,8 @@
 import * as firebase from 'firebase'
+import firebaseConfig from './firebaseConfig'
 
 require('firebase/firestore')
-
-// initialize firebase
-var firebaseConfig = {
-  apiKey: 'AIzaSyDi5gs5uJ-BQBnmOY3MxW56m_6GlnzKVDs',
-  authDomain: 'givecard-dcm.firebaseapp.com',
-  databaseURL: 'https://givecard-dcm.firebaseio.com',
-  projectId: 'givecard-dcm',
-  storageBucket: 'givecard-dcm.appspot.com',
-  messagingSenderId: '252395130688',
-  appId: '1:252395130688:web:e17255fe0353cefefbb4a5',
-  measurementId: 'G-ZRXDQVKKPB'
-}
-
+//initialize firebase
 firebase.initializeApp(firebaseConfig)
-
 
 export default firebase
