e801dda08eae1b98320fe2a9ff9d1515d146e68d
Will Peters <peterswi@bc.edu>
2020-06-24 14:33:30 -0400

    resources page added in prep for new cards

---
+++ src/components/resources.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Resources = () => {
+  return (
+    <div>
+      <h1>Resource Page</h1>
+      <p>Click one of the resource cards for more help</p>
+    </div>
+  )
+}
+
+export default Resources
