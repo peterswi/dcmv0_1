9da8da9c6bd0d3dc4443dc2c4e06457698bf534f
Will Peters <peterswi@bc.edu>
2020-07-01 16:47:22 -0400

    hung up on one piece of authentication before completion of this spec

--- src/components/orgs/pineStreetInn.js
+++ src/components/orgs/pineStreetInn.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import '../App.css'
 // import { makeStyles } from '@material-ui/core/styles'
 import OrgUser from '../orgUser'
+import {app} from '../../firestore'
+import * as firebase from 'firebase';
 
 /*
 const useStyles = makeStyles({
@@ -33,18 +35,56 @@ class PineStreet extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      orgId: 'PineStreetInn'
+      orgId: 'PineStreetInn',
+      admin: false
     }
   }
 
+  async checkAdmin(){
+    const user=app.auth().currentUser
+    const email=user.email
+    const db=firebase.firestore()
+    let admin=false
+    db.collection("users").where("email","==",email).get().then(function(querySnapshot){
+      querySnapshot.forEach(function(doc){
+        admin=doc.data().isAdmin
+        console.log("admin:",admin)
+      })
+
+      })
+  }
+
   render () {
+    //need to figure out how to grab the snapshot data from above and then use it down here for authentication
+    console.log(this.checkAdmin())
+    if(true){
+      return (
+        <div className='App'>
+          <header className='App-header'>
+            <h2><b> The Pine Street Inn</b></h2>
+          </header>
+          <body className='App-page'>
+
+          <div>
+            Admin: Add User Here <b /> <OrgUser orgId={this.state.orgId} />
+          </div>
+
+          </body>
+        </div>
+      )
+
+    }
     return (
       <div className='App'>
         <header className='App-header'>
           <h2><b> The Pine Street Inn</b></h2>
         </header>
         <body className='App-page'>
-        Admin: Add User Here <b /> <OrgUser orgId={this.state.orgId} />
+
+        <div>
+          This is the content for the Pine Street Inn's Page
+        </div>
+
         </body>
       </div>
     )
