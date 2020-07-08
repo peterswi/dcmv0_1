06230f15c4d997bcaa3c3513723e0d3fe2ee58fc
Will Peters <peterswi@bc.edu>
2020-07-08 12:42:42 -0400

    cleaning

--- src/index.js
+++ src/index.js
@@ -4,22 +4,9 @@ import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
-// import * as firebase from 'firebase'
 
-/*
-var firebaseConfig = {
-    apiKey: "AIzaSyDi5gs5uJ-BQBnmOY3MxW56m_6GlnzKVDs",
-    authDomain: "givecard-dcm.firebaseapp.com",
-    databaseURL: "https://givecard-dcm.firebaseio.com",
-    projectId: "givecard-dcm",
-    storageBucket: "givecard-dcm.appspot.com",
-    messagingSenderId: "252395130688",
-    appId: "1:252395130688:web:e17255fe0353cefefbb4a5",
-    measurementId: "G-ZRXDQVKKPB"
-  };
 
-firebase.initializeApp(firebaseConfig);
-*/
+
 
 ReactDOM.render(
   <React.StrictMode>
