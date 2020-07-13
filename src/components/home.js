cdea1e585cefbd75da785452e74b9d3a8651ba33
Will Peters <peterswi@bc.edu>
2020-07-13 16:16:40 -0400

    styling the homepage as an extension of familazrization with bulma

--- src/components/home.js
+++ src/components/home.js
@@ -2,40 +2,19 @@ import React from 'react'
 import logo from './logo.png'
 import './App.css'
 import { Link } from 'react-router-dom'
-import Button from '@material-ui/core/Button'
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import { makeStyles } from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
-import { useStyles } from './classes'
+
+// all of these imports were from MUI, have been replaced by better looking bulma stuff lol
+// import Button from '@material-ui/core/Button' no longer using MUI buttons
+// import Card from '@material-ui/core/Card'
+// import CardActions from '@material-ui/core/CardActions'
+// import CardContent from '@material-ui/core/CardContent'
+// import { makeStyles } from '@material-ui/core/styles'
+// import Typography from '@material-ui/core/Typography'
+// import { useStyles } from './classes'
 
 import 'bulma/css/bulma.css'
 import './sass/mystyles.scss'
 
-const mystyles = makeStyles({
-  root: {
-    minWidth: 275,
-    float: 'left',
-    margin: 'auto'
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)'
-  },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
-  },
-  center: {
-    margin: 'auto',
-    width: '50%',
-    padding: '10px'
-  }
-})
 
 // NOTES: Need to figure out coloring and the classnames for the full page and the header
 
@@ -46,10 +25,10 @@ export default function Home () {
   return (
     <div className='has-text-centered'>
 
-      <header className='App-header'>
+      <header className='heading is-family-secondary'>
         <img src={logo} className='App-logo' alt='logo' />
-        <h1> GiveCard Presents: GiveHelp</h1>
-        <h2> A tool for Case Management</h2>
+        <h1 className="title"> GiveCard Presents: GiveHelp</h1>
+        <h2 className="subtitle"> A tool for Case Management</h2>
         <p />
       </header>
 
@@ -59,10 +38,10 @@ export default function Home () {
           <h2 className="title"><b> Click a link to visit our different pages</b></h2>
           <div className="columns">
             <div className="column">
-              <Button variant='contained' color='white' href='/orgPage'> Visit an Organization Page</Button>
+              <a className="button is-hoverable is-info" href='/orgPage'> Visit an Organization Page</a>
             </div>
             <div className="column">
-              <Button variant='contained' color='white' href='/resources'> View All Resources</Button></div>
+              <a className='button is-hoverable is-primary'  href='/resources'> View All Resources</a></div>
           </div>
         </div>
       </section>
@@ -70,74 +49,83 @@ export default function Home () {
       <section className="section">
         <div className="columns">
           <div className="column">
-            <Card style={{ minWidth: 275, float: 'left', margin: 'auto' }} raised>
-              <CardContent>
-                <Typography className={useStyles.title} color='textSecondary' gutterBottom>
-                    COVID-19
-                </Typography>
-                <Typography variant='h5' component='h2'>
-                    Get CoronaVirus Support
-                </Typography>
-              </CardContent>
-              <CardActions className={useStyles.center}>
-                <Button variant='contained' href='/covid'> Learn More </Button>
-              </CardActions>
-            </Card>
+            <div className="card">
+              <header className="card-header">
+                <p className="card-header-title is-centered">
+                  COVID-19
+                </p>
+              </header>
+              <div className="card-content">
+                <div className="content">
+                  Find information about resources for Coronavirus relief.
+                </div>
+              </div>
+              <footer className="card-footer">
+                <a className="card-footer-item" href='/covid'> Learn More </a>
+              </footer>
+            
+            </div>
           </div>
           <div className="column">
-            <Card style={{ minWidth: 275, float: 'left', margin: 'auto' }} raised>
-              <CardContent>
-                <Typography className={useStyles.title} color='textSecondary' gutterBottom>
-                    Housing
-                </Typography>
-                <Typography variant='h5' component='h2'>
-                    Get Housing Support
-                </Typography>
-              </CardContent>
-              <CardActions className={useStyles.center}>
-                <Button variant='contained' href='/housing'> Learn More </Button>
-              </CardActions>
-            </Card>
+            <div className="card">
+              <header className="card-header">
+                <p className="card-header-title is-centered">
+                  Housing
+                </p>
+              </header>
+              <div className="card-content">
+                <div className="content">
+                  See resources available related to housing.
+                </div>
+              </div>
+              <footer className="card-footer">
+                <a className="card-footer-item" href='/housing'> Learn More </a>
+              </footer>
+            </div>
           </div>
           <div className="column">
-            <Card style={{ minWidth: 275, float: 'left', margin: 'auto' }} raised>
-              <CardContent>
-                <Typography className={useStyles.title} color='textSecondary' gutterBottom>
-                    Food
-                </Typography>
-                <Typography variant='h5' component='h2'>
-                    Get Food Support
-                </Typography>
-              </CardContent>
-              <CardActions className={useStyles.center}>
-                <Button variant='contained' href='/food'> Learn More </Button>
-              </CardActions>
-            </Card>
+            <div className="card">
+              <header className="card-header">
+                <p className="card-header-title is-centered">
+                  Food
+                </p>
+              </header>
+              <div className="card-content">
+                <div className="content">
+                  Get connected to food-related resources.
+                </div>
+              </div>
+              <footer className="card-footer">
+                <a className="card-footer-item" href='/food'> Learn More </a>
+              </footer>
+            </div>
           </div>
           <div className="column">
-            <Card style={{ minWidth: 275, float: 'left', margin: 'auto' }} raised>
-              <CardContent>
-                <Typography className={useStyles.title} color='textSecondary' gutterBottom>
-                    Jobs
-                </Typography>
-                <Typography variant='h5' component='h2'>
-                    Get Job Search Support
-                </Typography>
-              </CardContent>
-              <CardActions className={useStyles.center}>
-                <Button variant='contained' href='/job'> Learn More </Button>
-              </CardActions>
-            </Card>
+            <div className="card">
+              <header className="card-header">
+                <p className="card-header-title is-centered">
+                  Jobs
+                </p>
+              </header>
+              <div className="card-content">
+                <div className="content">
+                  Find resources relating to employment and the job search.
+                </div>
+              </div>
+              <footer className="card-footer">
+                <a className="card-footer-item" href='/job'> Learn More </a>
+              </footer>
+            </div>
           </div>
         </div>
       </section>
 
       </body>
 
-      <footer className='footer'>
-        <div class="content has-text-centered">
+      <footer className='footer has-background-success'>
+        <div className="content has-text-centered">
         <a
-          className='App-link'
+          className='is-link'
           href='https://www.givecard.io/'
           target='_blank'
           rel='noopener noreferrer'
