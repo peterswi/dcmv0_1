4d698c9fb05476e665e3c74d48af24388b13c3d0
Will Peters <peterswi@bc.edu>
2020-07-14 16:11:01 -0400

    going wild with styling! I love bulma

--- src/components/orgPage.js
+++ src/components/orgPage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from './logo.png'
 import './App.css'
 import 'bulma/css/bulma.css'
 import './sass/mystyles.scss'
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import './App.css'
 import { app } from '../firestore'
-import Button from '@material-ui/core/Button'
-import { Link } from 'react-router-dom'
+
 import 'bulma/css/bulma.css'
 import './sass/mystyles.scss'
 
